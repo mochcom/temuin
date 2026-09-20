c07f666ab63edb4d9c8b845a89a9094b4c73df53
hendyazzamuddin-dsgn <hendy.azzamuddin@gmail.com>
2026-09-20 21:10:10 +0700

    Delete temuin-app/temuin-app directory

--- temuin-app/temuin-app/backend/src/routes/reportRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const reportController = require("../controllers/reportController");
-
-// POST /api/report → kirim Formulir Lapor Cepat
-router.post("/", reportController.createReport);
-
-module.exports = router;
